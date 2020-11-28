31c351aa5abd0799f074bff53abc65dea416d868
albchia <albertichiara@hotmail.com>
2020-11-28 16:38:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let seconds = 00;
 let tens = 00;
 let appendTens = document.getElementById("tens");
+let appendSeconds = document.getElementById("seconds");
+let buttonStart = document.getElementById("buttonStart");
+let buttonStop = document.getElementById("buttonStop");
